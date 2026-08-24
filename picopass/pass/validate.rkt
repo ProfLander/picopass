31d12e3409147cc891d7045ee226192df97ee77e
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-08-24 18:43:23 +0100

    Implement pass input / output non-terminal validation

--- picopass/pass/validate.rkt
+++ picopass/pass/validate.rkt
@@ -22,161 +22,207 @@
 
 (provide (all-defined-out))
 
-(define (validate-pass self)
+(define (validate-pass pass)
   (-> pass? (or/c pass? none/c))
   "ensure SELF is a valid pass"
 
-  (~> self
+  (~> pass
       (validate-pass/bound-input)
       (validate-pass/bound-output)
       (validate-pass/unique-processors)
+      (validate-pass/input-coverage)
+      (validate-pass/output-coverage)
       (validate-pass/valid-processors)))
 
-(define (validate-pass/bound-input self)
+(define (validate-pass/bound-input pass)
   (-> pass? (or/c pass? none/c))
   "ensure the input of SELF is a valid binding"
 
-  (let ([input (pass-input self)])
+  (let ([input (pass-input pass)])
     (unless (language? input)
       (unless (identifier-binding input)
-        [raise-pass-error self
+        [raise-pass-error pass
          "unbound pass input"
          input])))
 
-  self)
+  pass)
 
-(define (validate-pass/bound-output self)
+(define (validate-pass/bound-output pass)
   (-> pass? (or/c pass? none/c))
   "ensure the output of SELF is a valid binding"
 
-  (let ([input (pass-input self)])
+  (let ([input (pass-input pass)])
     (unless (language? input)
       (unless (identifier-binding input)
-        [raise-pass-error self
+        [raise-pass-error pass
          "unbound pass input"
          input]))
 
-    self))
+    pass))
 
-(define (validate-pass/unique-processors self)
+(define (validate-pass/unique-processors pass)
   (-> pass? (or/c pass? none/c))
   "ensure no duplicate processors are present in SELF"
-  (let* ([processors (pass-processors self)]
+  (let* ([processors (pass-processors pass)]
          [processor-idents (map processor-ident processors)]
          [duplicate (check-duplicates processor-idents datum=?)])
     (when duplicate
-      [raise-pass-error self
+      [raise-pass-error pass
        "duplicate processor name"
        duplicate])
-    self))
+    pass))
 
-(define (validate-pass/valid-processors self)
+(define (validate-pass/input-coverage pass)
+  (-> pass? (or/c pass? none/c))
+  "when the input of SELF is a language and the output is not,
+   (i.e. automatic clause generation is not taking place,)
+   ensure each of its non-terminals is the input of at least one processor"
+  (let ([input (pass-input pass)] 
+        [output (pass-output pass)])
+
+    (when (language? input)
+      (unless (language? output)
+        (let ([non-terminals (language-non-terminals input)])
+          (for ([non-terminal (in-list non-terminals)])
+            (unless (for/or ([processor (in-list (pass-processors pass))])
+                      (non-terminal=? non-terminal (processor-input processor)))
+              [raise-pass-error pass
+               (format "~a non-terminal ~a is not the input of any processor"
+                       (language-name input)
+                       (non-terminal-name non-terminal))])))))
+
+    pass))
+
+(define (validate-pass/output-coverage pass)
+  (-> pass? (or/c pass? none/c))
+  "when the output of SELF is a language and the input is not,
+   (i.e. automatic clause generation is not taking place,)
+   ensure each of its non-terminals is the output of at least one processor"
+
+  (let ([input (pass-input pass)] 
+        [output (pass-output pass)])
+    (when (language? output)
+      (unless (language? input)
+        (let ([non-terminals (language-non-terminals output)])
+          (for ([non-terminal (in-list non-terminals)])
+            (unless (for/or ([processor (in-list (pass-processors pass))])
+                      (non-terminal=? non-terminal (processor-output processor)))
+              [raise-pass-error pass
+               (format "~a non-terminal ~a is not output by any processor"
+                       (language-name output)
+                       (non-terminal-name non-terminal))])))))
+
+    pass))
+
+(define (validate-pass/valid-processors pass)
   (-> pass? (or/c pass? none/c))
   "ensure the processors in SELF are all valid"
-  (let ([processors (pass-processors self)])
-    (map (curryr validate-processor self) processors)
-    self))
+  (let ([processors (pass-processors pass)])
+    (for ([processor (in-list processors)])
+      (validate-processor pass processor))
+    pass))
 
-(define (validate-processor self pass)
-  (-> processor? pass? (or/c processor? none/c))
+(define (validate-processor pass processor)
+  (-> pass? processor? (or/c processor? none/c))
   "ensure SELF is a valid processor"
-  (~> self
-      (validate-processor/bound-input _ pass)
-      (validate-processor/valid-input _ pass)
-      (validate-processor/bound-output _ pass)
-      (validate-processor/valid-output _ pass)
+  (~> processor
+      (validate-processor/bound-input pass _)
+      (validate-processor/valid-input pass _)
+      (validate-processor/bound-output pass _)
+      (validate-processor/valid-output pass _)
       (validate-processor/unique-clauses)
-      (validate-processor/valid-clauses _ pass)))
+      (validate-processor/valid-clauses pass _)))
 
-(define (validate-processor/bound-input self pass)
-  (-> processor? pass? (or/c processor? none/c))
+(define (validate-processor/bound-input pass processor)
+  (-> pass? processor? (or/c processor? none/c))
   "if pass input is a predicate, ensure the input of SELF is a valid binding"
 
   (let ([pass-input (pass-input pass)])
     (unless (language? pass-input)
-      (let ([input-ident (processor-input-ident self)])
+      (let ([input-ident (processor-input-ident processor)])
         (unless (identifier-binding input-ident)
-          [raise-processor-error self
+          [raise-processor-error processor
            "unbound processor input"
-           (processor-input-ident self)])))
+           (processor-input-ident processor)])))
 
-    self))
+    processor))
 
-(define (validate-processor/valid-input self pass)
-  (-> processor? pass? (or/c processor? none/c))
+(define (validate-processor/valid-input pass processor)
+  (-> pass? processor? (or/c processor? none/c))
   "if pass input is a language, ensure the input of SELF is a valid reference"
 
   (let ([pass-input (pass-input pass)])
     (when (language? pass-input)
-      (let ([input (processor-input self)])
+      (let ([input (processor-input processor)])
         (unless input
-          [raise-processor-error self
+          [raise-processor-error processor
            (format "invalid input non-terminal for language ~a"
                    (language-name pass-input))
-           (processor-input-ident self)])))
+           (processor-input-ident processor)])))
 
-    self))
+    processor))
 
-(define (validate-processor/bound-output self pass)
-  (-> processor? pass? (or/c processor? none/c))
+(define (validate-processor/bound-output pass processor)
+  (-> pass? processor? (or/c processor? none/c))
   "if pass input is a predicate, ensure the output of SELF is a valid binding"
 
   (let ([pass-output (pass-output pass)])
     (unless (language? pass-output)
-      (let ([output-ident (processor-output-ident self)])
+      (let ([output-ident (processor-output-ident processor)])
         (unless (identifier-binding output-ident)
-          [raise-processor-error self
+          [raise-processor-error processor
            "unbound processor output"
-           (processor-output-ident self)])))
+           (processor-output-ident processor)])))
 
-    self))
+    processor))
 
-(define (validate-processor/valid-output self pass)
-  (-> processor? pass? (or/c processor? none/c))
+(define (validate-processor/valid-output pass processor)
+  (-> pass? processor? (or/c processor? none/c))
   "if pass input is a language, ensure the output of SELF is a valid reference"
 
   (let ([pass-output (pass-output pass)])
     (when (language? pass-output)
-      (let ([output (processor-output self)])
+      (let ([output (processor-output processor)])
         (unless output
-          [raise-processor-error self
+          [raise-processor-error processor
            (format "invalid output non-terminal for language ~a"
                    (language-name pass-output))
-           (processor-output-ident self)])))
+           (processor-output-ident processor)])))
 
-    self))
+    processor))
 
-(define (validate-processor/unique-clauses self)
+(define (validate-processor/unique-clauses processor)
   (-> processor? (or/c processor? none/c))
   "ensure no duplicate clauses are present in SELF"
 
-  (let* ([clauses (processor-clauses self)]
+  (let* ([clauses (processor-clauses processor)]
          [patterns (map processor-clause-pattern clauses)]
          [duplicate (check-duplicates patterns pattern=?)])
 
     (when duplicate
-      [raise-processor-error self
+      [raise-processor-error processor
        "duplicate processor clause"
        (pattern-stx duplicate)])
 
-    self))
+    processor))
 
-(define (validate-processor/valid-clauses self pass)
-  (-> processor? pass? (or/c processor? none/c))
+(define (validate-processor/valid-clauses pass processor)
+  (-> pass? processor? (or/c processor? none/c))
   "ensure the clauses in SELF are all valid"
-  (let ([clauses (processor-clauses self)])
-    (map (curryr validate-processor-clause pass self) clauses)
-    self))
+  (let ([clauses (processor-clauses processor)])
+    (for ([clause (in-list clauses)])
+      (validate-processor-clause pass processor clause))
+    processor))
 
-(define (validate-processor-clause self pass processor)
-  (-> processor-clause? pass? processor? processor-clause?)
+(define (validate-processor-clause pass processor clause)
+  (-> pass? processor? processor-clause? processor-clause?)
   "ensure SELF is a valid processor clause"
 
-  (~> self
-      (validate-processor-clause/valid-pattern _ pass processor)))
+  (~> clause
+      (validate-processor-clause/valid-pattern pass processor _)))
 
-(define (validate-processor-clause/valid-pattern self pass processor)
-  (-> processor-clause? pass? processor? processor-clause?)
+(define (validate-processor-clause/valid-pattern pass processor clause)
+  (-> pass? processor? processor-clause? processor-clause?)
   "ensure the pattern of SELF corresponds to an input language non-terminal"
 
   (let* ([input (processor-input processor)])
@@ -184,13 +230,13 @@
     (when (non-terminal? input)
       (let* ([lang (pass-input pass)]
              [productions (non-terminal-productions input)]
-             [pattern (processor-clause-pattern self)]
+             [pattern (processor-clause-pattern clause)]
              [pattern (processor-clause-pattern->non-terminal-pattern pattern)])
 
         (unless (member pattern productions pattern=?)
-          [raise-processor-clause-invalid-pattern-error self
+          [raise-processor-clause-invalid-pattern-error clause
            (language-name lang)
            (pattern-stx pattern)])))
 
-    self))
+    clause))
 
