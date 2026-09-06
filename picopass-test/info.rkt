39b6aed2a571dd019514f58d1b8d82fa7073228f
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-09-06 21:37:44 +0100

    Remove compile-omit-paths from picopass-test

--- picopass-test/info.rkt
+++ picopass-test/info.rkt
@@ -3,5 +3,4 @@
 (define collection "picopass")
 (define deps '("base" "picopass-lib"))
 (define build-deps '("rackunit-lib"))
-(define compile-omit-paths '("tests"))
 (define clean '("compiled" "tests/compiled"))
