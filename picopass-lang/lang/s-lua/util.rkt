661e9b439923c8acb555e2e97ba57e838a812bd3
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-09-20 19:12:02 +0100

    s-lua: Implement utility module

---
+++ picopass-lang/lang/s-lua/util.rkt
@@ -0,0 +1,83 @@
+#lang racket/base
+
+(require syntax/parse
+         picopass/lang/s-lua/language)
+
+(provide (all-defined-out))
+
+(define (splice-requires requires body)
+
+  (define module-requires
+    (for/list ([pair (in-hash-keys requires)])
+      (list (string->symbol (cdr pair)) (car pair))))
+
+  (define member-requires
+    (for/list ([(pair reqs) (in-hash requires)])
+      (let ([name (string->symbol (cdr pair))])
+        (map (lambda (req)
+               (list name req))
+             reqs))))
+
+  (define module-binding
+    (if (pair? module-requires)
+        (with-syntax ([([require-target require-target-str] ...)
+                       module-requires])
+          #'[(local [require-target ...]
+               [(require require-target-str) ...])])
+        #'[]))
+
+  (define member-binding
+    (if (pair? member-requires)
+        (with-syntax ([([[require-id-target require-id] ...] ...)
+                       member-requires])
+          #'[(local [require-id ... ...]
+               [(-> require-id-target require-id) ... ...])])
+        #'[]))
+
+  (syntax-parse body
+    [((~datum #%chunk)
+      ((~datum #%block)
+       body ...))
+     (with-syntax
+       ([(module-binding ...) module-binding]
+        [(member-binding ...) member-binding])
+
+       #'(#%chunk
+          (#%block
+           module-binding ...
+           member-binding ...
+           body ...)))]))
+
+(define (splice-provides provides body)
+  "Splice PROVIDES into BODY as a return table"
+
+  (syntax-parse body
+    [((~datum #%chunk)
+      ((~datum #%block)
+       body ...))
+     (with-syntax
+       ([(module-provide ...) provides])
+
+       #'(#%chunk
+          (#%block
+
+           body ...
+
+           (return (table
+                    [module-provide module-provide]
+                    ...)))))]))
+
+(define (make-package-preloader name body)
+  "Return an S-Lua statement that injects BODY into package.preload
+   as a module with NAME"
+
+  (syntax-parse body
+    [((~datum #%chunk)
+      ((~datum #%block) body ...))
+     (with-syntax ([ooo (quote-syntax ...)])
+
+       #`(= [(-> (-> package preload) #,name)]
+            [(function (ooo)
+                       (#%block
+                        body ...))]))]))
+
