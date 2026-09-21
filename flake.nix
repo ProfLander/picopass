dc0faf0f2913437d7e88ace908e086cc77facaeb
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-09-21 18:00:00 +0100

    Integrate `elan` emacs config

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    elan.url = "github:ProfLander/elan";
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, elan, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -12,6 +13,7 @@
     {
       devShells.${system}.default = pkgs.mkShell {
         packages = [
+          elan.packages.${system}.default
           pkgs.racket
         ];
       };
